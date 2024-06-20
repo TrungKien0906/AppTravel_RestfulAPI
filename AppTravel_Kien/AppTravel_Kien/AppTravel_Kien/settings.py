0d05a1cf27b618e2191a9ac077cc4c90c861f9d1
TrungKien0906 <119091556+TrungKien0906@users.noreply.github.com>
2024-06-20 09:34:35 +0700

    Update settings.py

--- AppTravel_Kien/AppTravel_Kien/AppTravel_Kien/settings.py
+++ AppTravel_Kien/AppTravel_Kien/AppTravel_Kien/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-g*8d)l_(psw*(8zhg_4tft1ap*x$b7uqs8o6ge-70_x$2%9x$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
